200d0cb674b3d07846aeb32eab8b16aaa0bcd750
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-04-10 15:34:36 -0400

    blyat

--- src/setUp.groovy
+++ src/setUp.groovy
@@ -1,4 +1,4 @@
-def notify() {
+def call() {
     office365ConnectorSend color: '05b222',
         message: "Started build. Branch: ${BRANCH} :: Environment: ${ENVIRONMENT} :: Repo: ${CODE_URL} :: Node: ${NODE}",
         webhookUrl: "${TEAMS_HOOK}"
