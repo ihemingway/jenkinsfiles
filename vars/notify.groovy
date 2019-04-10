d97e27e09a9d352266e6b9b9fe5ef81b0d0c5193
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-04-10 15:52:14 -0400

    cleanup

--- vars/notify.groovy
+++
@@ -1,5 +0,0 @@
-def call() {
-    office365ConnectorSend color: '05b222',
-        message: "Started build. Branch: ${BRANCH} :: Environment: ${ENVIRONMENT} :: Repo: ${CODE_URL} :: Node: ${NODE}",
-        webhookUrl: "${TEAMS_HOOK}"
-}
